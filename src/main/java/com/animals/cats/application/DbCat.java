8e54f6bdb5be3acbcd850a67e7b1c00741c257a9
Aleksandra Mokrzycka <andrewblow@gmail.com>
2019-06-29 19:35:17 +0200

    database connected, db objects prepared

--- src/main/java/com/animals/cats/application/DbCat.java
+++ src/main/java/com/animals/cats/application/DbCat.java
@@ -1,21 +1,37 @@
 package com.animals.cats.application;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
 @Builder
+@Table (name = "cats")
+@Entity
+@RequiredArgsConstructor
 public class DbCat {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private final Long id;
+    @Column(name = "name", nullable = false)
     private final String name;
+    @Column (name = "birth_date")
     private final LocalDate birthDate;
+    @Column (name = "height_centimeters")
     private final Integer height;
+    @Column (name = "weight_kilograms")
     private final Double weight;
+    @Column (name = "eye_color")
     private final String eyeColor;
+    @Column (name = "fur_color")
     private final String furColor;
+    @Column (name = "personality")
     private final String personality;
+    @Column (name = "if_polite")
     private final Boolean polite;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn (name = "race_id")
     private final DbRace dbRace;
 }
--- src/main/java/com/animals/cats/application/DbRace.java
+++ src/main/java/com/animals/cats/application/DbRace.java
@@ -2,10 +2,20 @@ package com.animals.cats.application;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
 
 @Builder
 @Data
+@Entity
+@RequiredArgsConstructor
+@Table (name = "cat_races")
 public class DbRace {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private final Integer id;
+    @Column(name = "race_name", nullable = false, unique = true)
     private final String raceName;
 }
